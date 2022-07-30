f3729427adf4db010dded971abfc215ea708071d
Alex Sifel <alex.sifel@gmail.com>
2022-07-30 03:00:33 +0200

    Refactor

--- scripts/install_edgetpu_compiler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# For compiling a fully 8-Bit quantized TFLite model for Edge TPU
-# Usage: edgetpu_compiler model.tflite
-# Refer to https://coral.ai/docs/edgetpu/compiler/#usage for details
-
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add 
-echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | tee /etc/apt/sources.list.d/coral-edgetpu.list
-apt-get update
-apt-get install edgetpu-compiler
--- scripts/install_tflite_runtime_raspi4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Install tflite interpreter for Raspberry Pi 4 with Debian Buster (Python 3.7)
-# Refer to https://www.tensorflow.org/lite/guide/python for different environments
-
-pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl
